c6a8b25937b86b66619a3c1a246bf7cc5df0e144
Batbaatar99 <146710968+Batbaatar99@users.noreply.github.com>
2023-10-23 18:42:28 +0800

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+checkinputValue(document.getElementById("yourInputField").value);
+function checkinputValues(inputValue){
+if (inputValue === "") {
+    alert("Please enter a value. This field cannot be epmty.");
+} else if (isNaN(inputValue)){
+    alert("Please enter a valid number.");
+} else if (inputValue < 0){
+    alert ("Please enter a positive number.");
+}
+}

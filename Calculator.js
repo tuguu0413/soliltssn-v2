7c5f571ef49f1c42af1ae22df684a45121545beb
tuguu0413 <146718055+tuguu0413@users.noreply.github.com>
2023-10-29 11:11:10 +0800

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -11,3 +11,8 @@ if (inputValue === "") {
 document.getElementById("yourButton").addEventListener("click",function(event){
     event.preventDefault();// тоог 0-т хувааж болохгүй.
 });
+function toggleDarkMode() {
+    window.location.href = 'darkmode/main_dm.html';
+}
+
+document.getElementById('darkModeButton').addEventListener('click', toggleDarkMode);
